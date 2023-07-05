e3425649fd7bedd6ebdddab0222973c456d40fee
zoutianyu <46480905+modem1024k@users.noreply.github.com>
2023-07-05 15:33:32 +0800

    Add files via upload

---
+++ html-1.py
@@ -0,0 +1,22 @@
+
+import streamlit_echarts
+from pyecharts.charts import Line
+from pyecharts import options as opts
+import streamlit.components.v1 as components        #将要展示的 弄成html
+import streamlit as st
+import pandas as pd
+
+
+
+
+# 读取包含 Java 的 HTML 文件
+with open("my_new_驾驶舱.html", "r",encoding="utf-8") as f:
+    page_html = f.read()
+
+# 将 HTML 页面作为组件显示在 Streamlit 上
+#st.components.v1.html(page_html, width=700, height=500, scrolling=True)
+components .html(page_html, width=2400, height=3500, scrolling=True)
+#text=""
+#with open("my_new_驾驶舱.html",encoding="utf-8",width=00, height=7200, scrolling=True) as fp: #如果遇到decode错误，就加上合适的encoding
+    #text=fp.read()
+#components.html(text)
