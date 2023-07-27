737d63b502d5c4ce031abf27c7fd61fd60264bab
zoutianyu <46480905+modem1024k@users.noreply.github.com>
2023-07-27 12:36:04 +0800

    Update excel-1.py

--- excel-1.py
+++ excel-1.py
@@ -2,8 +2,6 @@ import pandas as pd
 import streamlit as st
 import xlrd
 
-
-
 st.set_page_config(page_title="My App", page_icon=":smiley:", layout="wide")
 rq = st.text_input('查询日期',"202306")
 
@@ -19,15 +17,7 @@ if st.button(' 业 务 查  询'):
         
 
     with col2:
-        df1 = file.parse(file.sheet_names[0])  #表名
-        st.write(df1,width=500, height=500, scrolling=True)
-
-        df2 = file.parse(file.sheet_names[1])  #表名
-        st.write(df2,width=500, height=500, scrolling=True)
-
-        df3 = file.parse(file.sheet_names[2])  #表名
-        st.write(df3,width=500, height=500, scrolling=True)
-
-        df4 = file.parse(file.sheet_names[3])  #表名
-        st.write(df4,width=500, height=500, scrolling=True)
+        for sheet in file.sheet_names:
+            df = file.parse(sheet)  #表名
+            st.write(df,width=500, height=500, scrolling=True)
 
